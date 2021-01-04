1db040f9ff23d8808df8770bc9d0f51a0e50dd8a
Juan <juanoa.otalora@gmail.com>
2021-01-04 20:06:17 +0100

    404 pagina

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,17 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import sad from '../images/sad.png'
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="page-content text-center mt-5 mb-5">
+        <h1>Error 404: página no encontrada</h1>
+        <p>Vaya... Parece que has intentado acceder a una página que ya no existe o se ha movido de sitio. Mis disculpas.</p>
+        <img src={sad} width="300px"  alt="sad"/>
+        <p><a className="btn btn-outline-secondary" href="/">Ir a la página principal</a></p>
+    </div>
   </Layout>
 )
 

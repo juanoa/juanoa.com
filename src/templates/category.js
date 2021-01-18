7ec7ef862f8a1210a98d7ec0fc34a6d204e6ee95
Juan Otálora Alarcón <juan@juanoa.com>
2021-01-18 17:35:26 +0100

    Paginación en categorias

--- src/templates/category.js
+++ src/templates/category.js
@@ -2,23 +2,41 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from "../components/seo"
-import { Row } from "react-bootstrap"
+import { Pagination, Row } from "react-bootstrap"
 import CategoryItemGrid from "../components/categories/categoryItemGrid"
 
-const CategoryTemplate = ({ data }) => (
-  <Layout>
-    <SEO title={data.strapiCategory.title} description={data.strapiCategory.description?.slice(0, 140)} />
+const CategoryTemplate = ({ data }) => {
+  const postsPorPagina = 9;
+  const postsLength = data.strapiCategory.posts.length;
+
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const pagina = parseInt(urlParams.get('p') || 1);
+  const postInicio = postsPorPagina*(pagina-1);
+  const postFinal = postInicio+postsPorPagina;
+
+  const paginaSiguiente = postFinal<postsLength ? pagina+1 : pagina;
+  const paginaAnterior = pagina>1 ? pagina-1 : 1;
+
+  return (
+    <Layout>
+      <SEO title={data.strapiCategory.title} description={data.strapiCategory.description?.slice(0, 140)} />
       <div className="page-content">
-          <h1>{data.strapiCategory.title}</h1>
-          <p>{data.strapiCategory.description}</p>
-          <Row>
-              {data.strapiCategory.posts.reverse().map(post => (
-                <CategoryItemGrid post={post} categorySlug={data.strapiCategory.slug}/>
-              ))}
-          </Row>
+        <h1>{data.strapiCategory.title}</h1>
+        <p>{data.strapiCategory.description}</p>
+        <Row>
+          {data.strapiCategory.posts.reverse().slice(postInicio, postFinal).map(post => (
+            <CategoryItemGrid post={post} categorySlug={data.strapiCategory.slug}/>
+          ))}
+        </Row>
+        <Pagination>
+          <Pagination.Prev disabled={paginaAnterior === pagina} href={`/${data.strapiCategory.slug}?p=${paginaAnterior}`} />
+          <Pagination.Next disabled={paginaSiguiente === pagina} href={`/${data.strapiCategory.slug}?p=${paginaSiguiente}`} />
+        </Pagination>
       </div>
-  </Layout>
-)
+    </Layout>
+  )
+}
 
 export default CategoryTemplate
 

891ca7371ed82ff0384d8f662c6b72c2b662b563
Juan Otálora Alarcón <juan@juanoa.com>
2021-02-14 16:19:21 +0100

    Edit design of Book Post

--- src/components/books/bookPost.js
+++ src/components/books/bookPost.js
@@ -1,35 +1,27 @@
 import React from "react"
-import { FaAmazon } from "react-icons/all"
+import { FaAmazon } from "react-icons/all";
 
 const BookPost = ({book}) => {
 
   if (!book) return (<></>);
 
   return (
-    <div className="card border-0 bg-light shadow-sm mb-4">
-      <div className="card-body">
-        <div className="row">
-          <div className="col-3 book__post-column">
-            <img
-              src={book.coverPhoto.localFile.publicURL}
-              alt={book.title}
-            />
-          </div>
-          <div className="col-9 book__post-column">
-            <div className="book__title card-title h5">
-              {book.title}
-            </div>
-            <p className="book__description mt-2 card-text">
-              {book.description}
-            </p>
-            <a
-              className="btn btn-warning"
-              href={book.link}
-              rel="noreferrer nofollow"
-              target="_blank"
-            > <FaAmazon/> Comprar</a>
-          </div>
-        </div>
+    <div className="media bg-light p-4 mb-4 shadow-sm rounded">
+      <img
+        className="mr-3 book__post-image"
+        src={book.coverPhoto.localFile.publicURL}
+        alt={book.title}
+      />
+      <div className="media-body">
+        <h4 className="mt-0 mb-2 book__post-title text-right">Libro relacionado</h4>
+        <p className="mt-0 mb-1 font-weight-bold text-secondary">{book.title}</p>
+        <p className="book__post-description">{book.description}</p>
+        <a
+          className="btn btn-sm btn-warning mr-3"
+          href={book.link}
+          target="_blank"
+          rel="noreferrer nofollow"
+        ><FaAmazon /> Comprar</a>
       </div>
     </div>
   );

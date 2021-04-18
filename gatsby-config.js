e43a029ad5a66fde8e9b490ccec6df578991b4d8
Juan Otálora Alarcón <juan@juanoa.com>
2021-04-18 20:57:49 +0200

    Netlify Redirects

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,6 +61,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-sass`,
+    `gatsby-plugin-netlify`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,

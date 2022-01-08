6ab605a5068971c7b05351f2fd6316487315deae
Juan Otálora <juan@juanoa.com>
2022-01-08 12:03:10 +0100

    Blog as internal link

--- src/components/header/dropdownItem.js
+++ src/components/header/dropdownItem.js
@@ -1,14 +1,29 @@
 import React from 'react'
+import {Link} from "gatsby";
 
 export const DropdownItem = ({link}) => {
-  return (
-    <li>
-      <a className="dropdown-item" href={link.url}>
-        {
-          link.icon && <img src={link.icon} className="mr-2" height={12} alt={link.name} />
-        }
-        {link.name}
-      </a>
-    </li>
-  )
+    return (
+        <li>
+            {
+                link.internal
+                    ? (
+                        <Link className="dropdown-item" to={link.url}>
+                            {
+                                link.icon && <img src={link.icon} className="mr-2" height={12} alt={link.name}/>
+                            }
+                            {link.name}
+                        </Link>
+                    )
+                    : (
+                        <a className="dropdown-item" href={link.url} target="_blank">
+                            {
+                                link.icon && <img src={link.icon} className="mr-2" height={12} alt={link.name}/>
+                            }
+                            {link.name}
+                        </a>
+                    )
+            }
+
+        </li>
+    )
 }
